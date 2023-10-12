2b8b7158f3997c34f4b5718d9f442f88c009d981
eappiah7 <eappiah77@yahoo.com>
2023-10-11 22:58:06 -0400

    First draft done, autograder tests
    Multi-line description of commit,
    feel free to be detailed.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
 import {
-    Piano,
-    NoteInfo,
-    notes,
     NoteType,
     pianoClass
 } from "./models";
@@ -10,12 +7,6 @@ import "./style.css";
 const piano = new pianoClass();
 const pianoKeys = piano.pianoKeys;
 
-
-// function createPianoListener(piano : pianoClass) {
-//   const pianoContainerArr = document.getElementsByClassName("piano-container");
-//   const pianoContainer = pianoContainerArr[0];
-//   pianoContainer.addEventListener("click", handleKeyPress(piano));    
-// }
 function createPianoListener() {
   const pianoContainerArr = document.getElementsByClassName("piano-container");
   const pianoContainer = pianoContainerArr[0];
@@ -36,7 +27,35 @@ function playSound(note : HTMLElement) {
   const noteId : string = note.id;
   const key = (Object.keys(pianoKeys) as Array<NoteType>).find(key => pianoKeys[key].id === noteId);
   console.log(key);
-  pianoKeys[key!].sound.play();
+  const sound = pianoKeys[key!].sound;
+  // sound.play();
+
+
+  // // Pause and rewind the audio to the beginning
+  // sound.pause();
+  // //sound.load();
+  // sound.currentTime = 0;
+  
+  // // Play the audio
+  // sound.play();
+
+  var soundPromise = sound.play();
+
+  if (soundPromise !== undefined) {
+    soundPromise.then(()=> {
+      sound.play();
+      sound.pause();
+      sound.currentTime = 0;
+      sound.play(); 
+    })
+    // sound.play();
+    // sound.pause();
+    // sound.currentTime = 0;
+    // sound.play();
+    .catch(error =>{
+      console.error("Audio playback error:", error);
+    });
+  }
 }
 
 
