1e24655b46b3a8e7b5c44ed9d90eeac30c7b9c45
eappiah7 <eappiah77@yahoo.com>
2023-10-11 23:51:54 -0400

    check in with TA
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,49 @@
-import {
-  NoteType,
-  pianoClass
-} from "./models";
+import { NoteInfo, NoteType, PianoClass } from "./models";
 import "./style.css";
 
-const piano = new pianoClass();
-const pianoKeys = piano.pianoKeys;
+const piano: PianoClass = new PianoClass();
+const pianoKeys: Record<NoteType, NoteInfo> = piano.pianoKeys;
 
-function createPianoListener() {
-  const pianoContainerArr = document.getElementsByClassName("piano-container");
-  const pianoContainer = pianoContainerArr[0];
-  pianoContainer.addEventListener("click", handleKeyPress); 
+function createPianoListener(): void {
+  const pianoContainerArr: HTMLCollection =
+    document.getElementsByClassName("piano-container");
+  const pianoContainer: Element = pianoContainerArr[0];
+  pianoContainer.addEventListener("click", handleKeyPress);
 }
-function handleKeyPress(e: Event) {
-  e.preventDefault();
-  const target : HTMLElement = (e.target as HTMLElement)
 
-    if (target.tagName === "BUTTON") { 
+function handleKeyPress(e: Event): void {
+  e.preventDefault();
+  const target: HTMLElement = e.target as HTMLElement;
+  if (target.tagName === "BUTTON") {
     playSound(target);
   } else {
     return;
   }
-
 }
 
-function playSound(note : HTMLElement) {
-  const noteId : string = note.id;
-  const key  = (Object.keys(pianoKeys) as Array<NoteType>).find(key => pianoKeys[key].id === noteId);
-  const sound : HTMLAudioElement = pianoKeys[key!].sound;
+function playSound(note: HTMLElement): void {
+  const noteId: string = note.id;
+  const key = (Object.keys(pianoKeys) as Array<NoteType>).find(
+    (key) => pianoKeys[key].id === noteId,
+  ); //TODO how to get typescript type for this
+  const sound: HTMLAudioElement = pianoKeys[key!].sound;
 
-  var soundPromise : Promise<void> = sound.play();
+  var soundPromise: Promise<void> = sound.play();
   if (soundPromise !== undefined) {
-    soundPromise.then(()=> {
-      sound.play();
-      sound.pause();
-      sound.currentTime = 0;
-      sound.play(); 
-    })
-    .catch(error =>{
-      console.error("Audio playback error:", error);
-    });
+    soundPromise
+      .then(() => {
+        sound.play();
+        sound.pause();
+        sound.currentTime = 0;
+        sound.play();
+      })
+      .catch((error) => {
+        alert(
+          "Error with audio playback. Please look at the console for details and try again later.",
+        );
+        console.error("Audio playback error:", error.message);
+      });
   }
 }
 
 document.addEventListener("DOMContentLoaded", createPianoListener);
-
--- src/models.ts
+++ src/models.ts
@@ -4,10 +4,10 @@ export interface NoteInfo {
 }
 
 export interface Piano {
-  pianoKeys : Record<NoteType, NoteInfo>; 
+  pianoKeys: Record<NoteType, NoteInfo>;
 }
 
-export class pianoClass implements Piano{
+export class PianoClass implements Piano {
   pianoKeys: Record<NoteType, NoteInfo> = notes;
 }
 
@@ -15,18 +15,17 @@ type whiteKey = "A" | "B" | "C" | "D" | "E" | "F" | "G";
 type blackKey = "A-sharp" | "C-sharp" | "D-sharp" | "F-sharp" | "G-sharp";
 export type NoteType = whiteKey | blackKey;
 
-
- const notes : Record<NoteType, NoteInfo> = {
-    "A": { id: "A", sound: new Audio("/sounds/piano_A.mp3") },
-    "B": { id: "B", sound: new Audio("/sounds/piano_B.mp3") },
-    "C": { id: "C", sound: new Audio("/sounds/piano_C.mp3") },
-    "D": { id: "D", sound: new Audio("/sounds/piano_D.mp3") },
-    "E": { id: "E", sound: new Audio("/sounds/piano_E.mp3") },
-    "F": { id: "F", sound: new Audio("/sounds/piano_F.mp3") },
-    "G": { id: "G", sound: new Audio("/sounds/piano_G.mp3") },
-    "A-sharp": { id: "A-sharp", sound: new Audio("/sounds/piano_ASharp.mp3") },
-    "C-sharp": { id: "C-sharp", sound: new Audio("/sounds/piano_CSharp.mp3") },
-    "D-sharp": { id: "D-sharp", sound: new Audio("/sounds/piano_DSharp.mp3") },
-    "F-sharp": { id: "F-sharp", sound: new Audio("/sounds/piano_FSharp.mp3") },
-    "G-sharp": { id: "G-sharp", sound: new Audio("/sounds/piano_GSharp.mp3") }
-}
+const notes: Record<NoteType, NoteInfo> = {
+  A: { id: "A", sound: new Audio("/sounds/piano_A.mp3") },
+  B: { id: "B", sound: new Audio("/sounds/piano_B.mp3") },
+  C: { id: "C", sound: new Audio("/sounds/piano_C.mp3") },
+  D: { id: "D", sound: new Audio("/sounds/piano_D.mp3") },
+  E: { id: "E", sound: new Audio("/sounds/piano_E.mp3") },
+  F: { id: "F", sound: new Audio("/sounds/piano_F.mp3") },
+  G: { id: "G", sound: new Audio("/sounds/piano_G.mp3") },
+  "A-sharp": { id: "A-sharp", sound: new Audio("/sounds/piano_ASharp.mp3") },
+  "C-sharp": { id: "C-sharp", sound: new Audio("/sounds/piano_CSharp.mp3") },
+  "D-sharp": { id: "D-sharp", sound: new Audio("/sounds/piano_DSharp.mp3") },
+  "F-sharp": { id: "F-sharp", sound: new Audio("/sounds/piano_FSharp.mp3") },
+  "G-sharp": { id: "G-sharp", sound: new Audio("/sounds/piano_GSharp.mp3") },
+};
